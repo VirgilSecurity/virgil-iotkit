60e38c96f20a0215d2b6e039d92f628bec3fdd20
Dmytro Hrytsenko <dm.hrytsenko@gmail.com>
2019-03-13 16:37:09 +0200

    Delete key_storage abstract class, cleanup
    delete abstract class with one usage
    move some duplicated code to methods

--- tools/key-management/virgil_keymanager/storage/keys_tinydb_storage.py
+++ tools/key-management/virgil_keymanager/storage/keys_tinydb_storage.py
@@ -16,8 +16,8 @@ class KeysTinyDBStorage(object):
     def __compatibility(db_obj):
         return {str(db_obj.pop("key_id")): db_obj}
 
-    def save(self, key, value, suppress_db_warning=False):
-        db = TinyDB(
+    def _get_db(self, suppress_db_warning=False):
+        return TinyDB(
             self.storage_path,
             create_dirs=True,
             storage=self.__storage_type,
@@ -25,33 +25,22 @@ class KeysTinyDBStorage(object):
             suppress_db_warning=suppress_db_warning,
             **self.__storage_kwargs
         )
+
+    def save(self, key, value, suppress_db_warning=False):
+        db = self._get_db(suppress_db_warning)
         db.table(self.__table_name).insert({"key_id": key, **value})
         db.close()
 
     def get_keys(self, suppress_db_warning=False):
         def get_keys_id(element_list, curr_eid):
             return element_list[curr_eid]["key_id"]
-        db = TinyDB(
-            self.storage_path,
-            create_dirs=True,
-            storage=self.__storage_type,
-            default_table=self.__table_name,
-            suppress_db_warning=suppress_db_warning,
-            **self.__storage_kwargs
-        )
+        db = self._get_db(suppress_db_warning)
         keys = db.table(self.__table_name).search(where("key_id").exists())
         db.close()
         return list(map(lambda x: x["key_id"], keys))
 
     def get_values(self, suppress_db_warning=False):
-        db = TinyDB(
-            self.storage_path,
-            create_dirs=True,
-            storage=self.__storage_type,
-            default_table=self.__table_name,
-            suppress_db_warning=suppress_db_warning,
-            **self.__storage_kwargs
-        )
+        db = self._get_db(suppress_db_warning)
         values = db.table(self.__table_name).search(where("key_id").exists())
         db.close()
         for value in values:
@@ -60,14 +49,7 @@ class KeysTinyDBStorage(object):
 
     def get_all_data(self, suppress_db_warning=False):
         data = dict()
-        db = TinyDB(
-            self.storage_path,
-            storage=self.__storage_type,
-            create_dirs=True,
-            default_table=self.__table_name,
-            suppress_db_warning=suppress_db_warning,
-            **self.__storage_kwargs
-        )
+        db = self._get_db(suppress_db_warning)
         new_data = db.table(self.__table_name).all()
         for d in new_data:
             data.update(self.__compatibility(d))
@@ -75,14 +57,7 @@ class KeysTinyDBStorage(object):
         return data
 
     def get_value(self, key, suppress_db_warning=False):
-        db = TinyDB(
-            self.storage_path,
-            create_dirs=True,
-            storage=self.__storage_type,
-            default_table=self.__table_name,
-            suppress_db_warning=suppress_db_warning,
-            **self.__storage_kwargs
-        )
+        db = self._get_db(suppress_db_warning)
         data = db.table(self.__table_name).get(where("key_id") == str(key))
         if data:
             del data["key_id"]
@@ -90,37 +65,16 @@ class KeysTinyDBStorage(object):
         return data
 
     def delete_key(self, key, suppress_db_warning=False):
-        db = TinyDB(
-            self.storage_path,
-            create_dirs=True,
-            storage=self.__storage_type,
-            default_table=self.__table_name,
-            suppress_db_warning=suppress_db_warning,
-            **self.__storage_kwargs
-        )
+        db = self._get_db(suppress_db_warning)
         db.table(self.__table_name).remove(where("key_id") == str(key))
         db.close()
 
     def resign(self, suppress_db_warning=False):
         if self.__storage_type is SignedByteStorage or self.__storage_type is CryptoByteStorage:
-            db = TinyDB(
-                self.storage_path,
-                create_dirs=True,
-                storage=self.__storage_type,
-                default_table=self.__table_name,
-                suppress_db_warning=suppress_db_warning,
-                **self.__storage_kwargs
-            )
+            db = self._get_db(suppress_db_warning)
             db.table("resign")
             db.close()
 
-            db = TinyDB(
-                self.storage_path,
-                create_dirs=True,
-                storage=self.__storage_type,
-                default_table=self.__table_name,
-                suppress_db_warning=suppress_db_warning,
-                **self.__storage_kwargs
-            )
+            db = self._get_db(suppress_db_warning)
             db.purge_table("resign")
             db.close()
