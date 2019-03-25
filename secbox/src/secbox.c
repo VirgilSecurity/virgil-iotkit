4927cfefce8478335a7b0efa006995753fe30c0f
Roman Kutashenko <kutashenko@gmail.com>
2019-03-25 15:35:05 +0200

    [secbox] Add stub

--- secbox/src/secbox.c
+++ secbox/src/secbox.c
@@ -8,3 +8,24 @@
 #include <secbox.h>
 #include <iotelic_sp_interface.h>
 #include <sdmp/PRVS.h>
+
+
+int
+vs_secbox_configure_hal(const vs_secbox_hal_impl_t *impl) {
+    return -1;
+}
+
+int
+vs_secbox_save() {
+    return -1;
+}
+
+int
+vs_secbox_load() {
+    return -1;
+}
+
+int
+vs_secbox_del() {
+    return -1;
+}
