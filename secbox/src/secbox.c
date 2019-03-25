96a539fa5ed209e49b4adb71e69a534337c228dd
git <wizardiche@mail.ru>
2019-03-25 17:18:20 +0500

    Add secbox library to project

---
+++ secbox/src/secbox.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <secbox.h>
+#include <iotelic_sp_interface.h>
+#include <sdmp/PRVS.h>
