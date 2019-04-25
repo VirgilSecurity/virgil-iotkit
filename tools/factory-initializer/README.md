4703ccc030bd242307003d9e5a5c1fd091f5a7fc
Dmytro Hrytsenko <dm.hrytsenko@gmail.com>
2019-04-25 12:42:24 +0300

    [factory initializer] Remove C++ code

--- tools/factory-initializer/README.md
+++
@@ -1,2 +0,0 @@
-# virgil-iot-initializer
-The application to create a signed content snapshot for a Virgil Card creation
--- tools/factory-initializer/scripts/initialize-release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-"${SCRIPT_FOLDER}/init-devices.sh" $@ prod
