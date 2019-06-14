ede73b9c5d19e2ede2fce9c9a233d1fffe05c8b9
Oleksandr Nemchenko <avn@nemchenko.net>
2019-06-14 12:32:53 +0300

    Refactor keypair test

--- tests/src/crypto/keypair.c
+++ tests/src/crypto/keypair.c
@@ -3,18 +3,66 @@
 #include <virgil/iot/hsm/hsm_interface.h>
 #include <stdlib-config.h>
 
+typedef struct {
+    vs_iot_hsm_slot_e slot;
+    vs_hsm_keypair_type_e keypair_type;
+    const char *descr;
+    uint8_t *buf;
+    uint16_t key_sz;
+    uint16_t waited_size;
+} _test_case_t;
+
+/******************************************************************************/
+static bool
+_test_keypair_generate(_test_case_t *test_case){
+    vs_hsm_keypair_type_e keypair;
+
+    test_case->buf = NULL;
+    keypair = test_case->keypair_type;
+
+    VS_HSM_CHECK_RET(vs_hsm_keypair_create(test_case->slot, test_case->keypair_type), "vs_hsm_keypair_create call error");
+    VS_HSM_CHECK_RET(vs_hsm_keypair_get_pubkey(test_case->slot, &test_case->buf, sizeof(test_case->buf), &test_case->key_sz, &keypair), "vs_hsm_keypair_get_pubkey call error");
+    BOOL_CHECK_RET(keypair == test_case->keypair_type && test_case->buf != NULL && test_case->key_sz == test_case->waited_size, "Received buffers error");
+
+    // TODO : check sign/verify for keypair
+
+    return true;
+}
+
+/******************************************************************************/
+static bool
+_compare_outputs(_test_case_t *test_cases, size_t cases_amount) {
+
+    size_t pos;
+    size_t pos2;
+
+    for(pos = 0; pos < cases_amount; ++pos){
+        for(pos2 = pos + 1; pos2 < cases_amount; ++pos2){
+            if(test_cases[pos].key_sz != test_cases[pos2].key_sz){
+                continue;
+            }
+
+            if(VS_IOT_MEMCMP(test_cases[pos].buf, test_cases[pos2].buf, test_cases[pos].key_sz) == 0) {
+                VS_LOG_ERROR(
+                        "The same keys are generated for (slot = %d, key type = %d) and (slot = %d, key type = %d)",
+                        test_cases[pos].slot,
+                        test_cases[pos].keypair_type,
+                        test_cases[pos2].slot,
+                        test_cases[pos2].keypair_type);
+
+                return false;
+            }
+        }
+    }
+
+    return true;
+
+}
+
 /******************************************************************************/
 void
 test_keypair(void) {
 
-    typedef struct {
-        vs_iot_hsm_slot_e slot;
-        vs_hsm_keypair_type_e keypair_type;
-        const char *descr;
-        uint8_t *buf;
-        uint16_t key_sz;
-        uint16_t waited_size;
-    } _test_case_t;
 
     _test_case_t test_cases[] = {
 #if USE_RSA
@@ -33,39 +81,21 @@ test_keypair(void) {
     };
 
     static const size_t cases_amount = sizeof(test_cases) / sizeof(test_cases[0]);
-    vs_hsm_keypair_type_e keypair;
-    vs_iot_hsm_slot_e slot;
     size_t pos;
-    size_t pos2;
-    bool ok;
 
     START_TEST("Keypair tests");
 
     for(pos = 0; pos < cases_amount; ++pos){
         _test_case_t *test_case = &test_cases[pos];
 
-        keypair = test_case->keypair_type;
-        slot = test_case->slot;
-
-        TEST_CASE_OK(test_case->descr, VS_HSM_ERR_OK == vs_hsm_keypair_create(slot, keypair) &&
-                VS_HSM_ERR_OK == vs_hsm_keypair_get_pubkey(slot, &test_case->buf, sizeof(test_case->buf), &test_case->key_sz, &keypair) &&
-        keypair == test_case->keypair_type &&
-        test_case->buf != NULL && test_case->key_sz == test_case->waited_size);
+        TEST_CASE_OK(test_case->descr, _test_keypair_generate(test_case));
     }
 
-    ok = true;
-    for(pos = 0; pos < cases_amount && ok; ++pos){
-        for(pos2 = pos + 1; pos2 < cases_amount && ok; ++pos2){
-            ok = test_cases[pos].key_sz != test_cases[pos2].key_sz ||
-             VS_IOT_MEMCMP(test_cases[pos].buf, test_cases[pos2].buf, test_cases[pos].key_sz) != 0;
-        }
-    }
-
-    TEST_CASE_OK("Compare buffer outputs", ok);
+    TEST_CASE_OK("Compare buffer outputs", _compare_outputs(test_cases, cases_amount));
 
     terminate: ;
 
-    for(pos = 0; pos < cases_amount && ok; ++pos){
+    for(pos = 0; pos < cases_amount; ++pos){
         VS_IOT_FREE(test_cases[pos].buf);
     }
 
