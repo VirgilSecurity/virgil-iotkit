728ed8464217de41e0f7d0cde8189f323316d8fa
git <wizardiche@mail.ru>
2019-07-10 19:18:31 +0500

    Change test vector for aes256. Use vs_hsm_aes_auth_decrypt instead of vs_hsm_aes_decrypt

--- tests/src/crypto/aes.c
+++ tests/src/crypto/aes.c
@@ -40,16 +40,35 @@
 /******************************************************************************/
 static bool
 test_aes_cases(vs_iot_aes_type_e aes_type, bool test_implemented) {
+#if 0
     static const uint8_t source[] = "Input data to be crypted";
-    uint8_t crypted[sizeof(source)];
-    uint8_t decrypted[sizeof(source)];
-    uint8_t auth_decrypted[sizeof(source)];
     static const uint8_t iv[] = {0xca, 0xfe, 0xba, 0xbe, 0xfa, 0xce, 0xdb, 0xad, 0xde, 0xca, 0xf8, 0x88};
     static const uint8_t add[] = {0xfe, 0xed, 0xfa, 0xce, 0xde, 0xad, 0xbe, 0xef, 0xfe, 0xed,
                                   0xfa, 0xce, 0xde, 0xad, 0xbe, 0xef, 0xab, 0xad, 0xda, 0xd2};
-    uint8_t tag[16] = {0};
     static const uint8_t key[] = {
             0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+#endif
+    static const uint8_t source[] = {0xDE, 0xAD, 0xBE, 0xAF};
+    static const uint8_t iv[] = {0x78, 0xbe, 0xf6, 0x55, 0xdf, 0xd8, 0x99, 0x0b, 0x04, 0xd2, 0xa2, 0x56};
+    static const uint8_t add[] = {
+            0x9d, 0x8c, 0x67, 0x34, 0x54, 0x67, 0x97, 0xc5, 0x81, 0xb9, 0xb1, 0xd0, 0xd4, 0xf0, 0x5b, 0x27,
+            0xfe, 0x05, 0x39, 0xbd, 0x01, 0x65, 0x5d, 0x2d, 0x1a, 0x8a, 0x14, 0x89, 0xcd, 0xf8, 0x04, 0x22,
+            0x87, 0x53, 0xd7, 0x72, 0x72, 0xbf, 0x6d, 0xed, 0x19, 0xd4, 0x7a, 0x6a, 0xbd, 0x62, 0x81, 0xea,
+            0x95, 0x91, 0xd4, 0xbc, 0xc1, 0xbe, 0x22, 0x23, 0x05, 0xfd, 0xf6, 0x89, 0xc5, 0xfa, 0xa4, 0xc1,
+            0x13, 0x31, 0xcf, 0xfb, 0xf4, 0x22, 0x15, 0x46, 0x9b, 0x81, 0xf6, 0x1b, 0x40, 0x41, 0x5d, 0x81,
+            0xcc, 0x37, 0x16, 0x1e, 0x5c, 0x02, 0x58, 0xa6, 0x76, 0x42, 0xb9, 0xb8, 0xac, 0x62, 0x7d, 0x6e,
+            0x39, 0xf4, 0x3e, 0x48, 0x5e, 0x1f, 0xf5, 0x22, 0xac, 0x74, 0x2a, 0x07, 0xde, 0xfa, 0x35, 0x69,
+            0xae, 0xb5, 0x99, 0x90, 0xcb, 0x44, 0xc4, 0xf3, 0xd9, 0x52, 0xf8, 0x11, 0x9f, 0xf1, 0x11, 0x1d,
+    };
+    static const uint8_t key[] = {
+            0x43, 0xc9, 0xe2, 0x09, 0xda, 0x3c, 0x19, 0x71, 0xd9, 0x86, 0xa4, 0x5b, 0x92, 0xf2, 0xfa, 0x0d,
+            0x2d, 0x15, 0x51, 0x83, 0x73, 0x0d, 0x21, 0xd7, 0x1e, 0xd8, 0xe2, 0x28, 0x4e, 0xc3, 0x08, 0xe3,
+    };
+
+    uint8_t crypted[sizeof(source)];
+    uint8_t decrypted[sizeof(source)];
+    uint8_t auth_decrypted[sizeof(source)];
+    uint8_t tag[16] = {0};
     static const uint16_t data_sz = sizeof(source);
     static const uint16_t iv_sz = sizeof(iv);
     static const uint16_t add_sz = sizeof(add);
@@ -69,28 +88,31 @@ test_aes_cases(vs_iot_aes_type_e aes_type, bool test_implemented) {
                                                                 crypted,
                                                                 tag,
                                                                 tag_sz) &&
-               VS_HSM_ERR_NOT_IMPLEMENTED != vs_hsm_aes_decrypt(aes_type,
-                                                                key,
-                                                                key_bitsz,
-                                                                iv,
-                                                                iv_sz,
-                                                                add,
-                                                                add_sz,
-                                                                data_sz,
-                                                                crypted,
-                                                                decrypted,
-                                                                tag,
-                                                                tag_sz);
+               VS_HSM_ERR_NOT_IMPLEMENTED != vs_hsm_aes_auth_decrypt(aes_type,
+                                                                     key,
+                                                                     key_bitsz,
+                                                                     iv,
+                                                                     iv_sz,
+                                                                     add,
+                                                                     add_sz,
+                                                                     data_sz,
+                                                                     crypted,
+                                                                     decrypted,
+                                                                     tag,
+                                                                     tag_sz);
     } else {
 
         VS_HSM_CHECK_RET(
                 vs_hsm_aes_encrypt(
                         aes_type, key, key_bitsz, iv, iv_sz, add, add_sz, data_sz, source, crypted, tag, tag_sz),
                 "Unable to encrypt data");
+
+#if 0
         VS_HSM_CHECK_RET(
                 vs_hsm_aes_decrypt(
                         aes_type, key, key_bitsz, iv, iv_sz, add, add_sz, data_sz, crypted, decrypted, tag, tag_sz),
                 "Unable to decrypt data");
+#endif
         MEMCMP_CHECK_RET(source, decrypted, data_sz);
         VS_HSM_CHECK_RET(vs_hsm_aes_auth_decrypt(aes_type,
                                                  key,
