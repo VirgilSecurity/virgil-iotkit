c6031d20a92d7aebb9aa0ae3e69c14f33648997e
git <wizardiche@mail.ru>
2019-08-28 16:48:35 +0500

    clang format

--- hsm/src/hsm_virgil_ecies.c
+++ hsm/src/hsm_virgil_ecies.c
@@ -147,8 +147,7 @@ vs_hsm_virgil_decrypt_sha384_aes256(const uint8_t *recipient_id,
         return VS_HSM_ERR_CRYPTO;
     }
 
-    if (encrypted_data_sz < 16 ||
-            buf_sz < (encrypted_data_sz - 16)) {
+    if (encrypted_data_sz < 16 || buf_sz < (encrypted_data_sz - 16)) {
         return VS_HSM_ERR_INVAL;
     }
 
--- tests/src/crypto/secbox_test.c
+++ tests/src/crypto/secbox_test.c
@@ -14,7 +14,11 @@ static const char _little_test_data[] = "Little string for test";
 
 /******************************************************************************/
 static bool
-_test_case_secbox_save_load(vs_storage_op_ctx_t *ctx, const char *filename,  vs_secbox_type_t type, const char *test_data, uint32_t data_sz) {
+_test_case_secbox_save_load(vs_storage_op_ctx_t *ctx,
+                            const char *filename,
+                            vs_secbox_type_t type,
+                            const char *test_data,
+                            uint32_t data_sz) {
 
     uint8_t buf[data_sz];
     uint16_t hash_sz;
@@ -43,7 +47,8 @@ _test_case_secbox_del(vs_storage_op_ctx_t *ctx, const char *filename) {
 
     vs_hsm_hash_create(VS_HASH_SHA_256, (uint8_t *)filename, strlen(filename), file_id, sizeof(file_id), &hash_sz);
 
-    BOOL_CHECK_RET(_test_case_secbox_save_load(ctx, filename, VS_SECBOX_SIGNED, buf, strlen(buf)), "Error create file for delete test")
+    BOOL_CHECK_RET(_test_case_secbox_save_load(ctx, filename, VS_SECBOX_SIGNED, buf, strlen(buf)),
+                   "Error create file for delete test")
 
     BOOL_CHECK_RET(VS_STORAGE_OK == vs_secbox_del(ctx, file_id), "Error delete file")
 
@@ -67,7 +72,8 @@ vs_secbox_test(vs_storage_op_ctx_t *ctx) {
 
     TEST_CASE_OK(
             "Read/write small piece of data. Signed only",
-            _test_case_secbox_save_load(ctx, TEST_FILENAME_LITTLE_DATA, VS_SECBOX_SIGNED, _little_test_data, sizeof(_little_test_data)))
+            _test_case_secbox_save_load(
+                    ctx, TEST_FILENAME_LITTLE_DATA, VS_SECBOX_SIGNED, _little_test_data, sizeof(_little_test_data)))
 
     size_t big_test_size = (ctx->file_sz_limit > (3 * 256 + 128 - 1)) ? 3 * 256 + 128 - 1 : ctx->file_sz_limit;
     _big_test_data = VS_IOT_MALLOC(big_test_size);
@@ -75,17 +81,22 @@ vs_secbox_test(vs_storage_op_ctx_t *ctx) {
         _big_test_data[i] = (uint8_t)i;
     }
 
-    TEST_CASE_OK("Read/write big piece of data. Signed only",
-                 _test_case_secbox_save_load(ctx, TEST_FILENAME_BIG_DATA, VS_SECBOX_SIGNED, _big_test_data, big_test_size))
+    TEST_CASE_OK(
+            "Read/write big piece of data. Signed only",
+            _test_case_secbox_save_load(ctx, TEST_FILENAME_BIG_DATA, VS_SECBOX_SIGNED, _big_test_data, big_test_size))
 
     TEST_CASE_OK("Delete file", _test_case_secbox_del(ctx, TEST_FILENAME_FOR_DELETE))
 
-    TEST_CASE_OK(
-            "Read/write small piece of data. Signed and encrypted",
-            _test_case_secbox_save_load(ctx, TEST_FILENAME_LITTLE_DATA, VS_SECBOX_SIGNED_AND_ENCRYPTED, _little_test_data, sizeof(_little_test_data)))
+    TEST_CASE_OK("Read/write small piece of data. Signed and encrypted",
+                 _test_case_secbox_save_load(ctx,
+                                             TEST_FILENAME_LITTLE_DATA,
+                                             VS_SECBOX_SIGNED_AND_ENCRYPTED,
+                                             _little_test_data,
+                                             sizeof(_little_test_data)))
 
     TEST_CASE_OK("Read/write big piece of data.Signed and encrypted",
-                 _test_case_secbox_save_load(ctx, TEST_FILENAME_BIG_DATA, VS_SECBOX_SIGNED_AND_ENCRYPTED, _big_test_data, big_test_size))
+                 _test_case_secbox_save_load(
+                         ctx, TEST_FILENAME_BIG_DATA, VS_SECBOX_SIGNED_AND_ENCRYPTED, _big_test_data, big_test_size))
 terminate:;
     if (_big_test_data) {
         VS_IOT_FREE(_big_test_data);
