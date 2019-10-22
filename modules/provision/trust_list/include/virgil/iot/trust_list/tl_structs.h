d4e6890dac3fbd2913286b75a5e15491aec2f326
Oleksandr Nemchenko <avn@nemchenko.net>
2019-10-22 16:31:01 +0300

    [doc] Doxygen for Trust List

--- modules/provision/trust_list/include/virgil/iot/trust_list/tl_structs.h
+++ modules/provision/trust_list/include/virgil/iot/trust_list/tl_structs.h
@@ -1,3 +1,41 @@
+//  Copyright (C) 2015-2019 Virgil Security, Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+
+/*! \file tl_structs.h
+ * \brief Trust List structures
+ */
+
 #ifndef TL_STRUCTS_H
 #define TL_STRUCTS_H
 
@@ -5,22 +43,27 @@
 #include <stdbool.h>
 #include <virgil/iot/provision/provision.h>
 
-#define TL_STORAGE_TYPE_STATIC 0
-#define TL_STORAGE_TYPE_DYNAMIC 1
-#define TL_STORAGE_TYPE_TMP 2
+/** Trust List storage types */
+typedef enum {
+    TL_STORAGE_TYPE_STATIC = 0, /**< Default Trust List back up for restore it in case of provision error */
+    TL_STORAGE_TYPE_DYNAMIC, /**< Trust List during provision process */
+    TL_STORAGE_TYPE_TMP, /**< Trust list that is loading. After successful verification it is saved to \a TL_STORAGE_TYPE_STATIC */
+} vs_tl_storage_t;
 
 typedef size_t vs_tl_key_handle;
 
+/** Trust List header */
 typedef struct __attribute__((__packed__)) {
-    uint32_t tl_size; // header + public keys + footer
-    uint16_t version;
-    uint16_t pub_keys_count;
-    uint8_t signatures_count;
+    uint32_t tl_size; /**< Trust List size = header + public keys + footer */
+    uint16_t version; /**< Version */
+    uint16_t pub_keys_count; /**< Public keys amount */
+    uint8_t signatures_count; /**< Signatures amount */
 } vs_tl_header_t;
 
+/** Trust List footer */
 typedef struct __attribute__((__packed__)) {
-    uint8_t tl_type;
-    uint8_t signatures[];
+    uint8_t tl_type; /**< Trust List type */
+    uint8_t signatures[]; /**< Signatures */
 } vs_tl_footer_t;
 
 
--- modules/provision/trust_list/include/virgil/iot/trust_list/trust_list.h
+++ modules/provision/trust_list/include/virgil/iot/trust_list/trust_list.h
@@ -1,3 +1,40 @@
+//  Copyright (C) 2015-2019 Virgil Security, Inc.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//      (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//      (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in
+//      the documentation and/or other materials provided with the
+//      distribution.
+//
+//      (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+
+/*! \file trust_list.h
+ * \brief Trust List
+ */
 
 #ifndef TRUST_LIST_H
 #define TRUST_LIST_H
@@ -10,37 +47,83 @@
 #include <virgil/iot/update/update.h>
 #include <virgil/iot/trust_list/tl_structs.h>
 
+/** Trust List element type
+ *
+ * Used for save Trust List header, data or footer by \ref vs_tl_save_part call or load by \ref vs_tl_load_part call
+ */
 typedef enum {
     VS_TL_ELEMENT_MIN = 0,
-    VS_TL_ELEMENT_TLH,
-    VS_TL_ELEMENT_TLC,
-    VS_TL_ELEMENT_TLF,
+    VS_TL_ELEMENT_TLH, /**< Trust List header */
+    VS_TL_ELEMENT_TLC, /**< Trust List data chunk */
+    VS_TL_ELEMENT_TLF, /**< Trust List footer */
     VS_TL_ELEMENT_MAX,
 } vs_tl_element_e;
 
+/** Trust List element description */
 typedef struct vs_tl_element_info_s {
-    vs_tl_element_e id;
-    size_t index;
+    vs_tl_element_e id; /**< Trust List header, data chunk or footer selection */
+    size_t index; /**< Trust List data chunk number */
 } vs_tl_element_info_t;
 
+/** Trust List initialization
+ *
+ * \param[in] op_ctx \ref vs_storage_op_ctx_t storage context. Must not be NULL.
+ * \param[in] hsm \ref vs_hsm_impl_t HSM implementation. Must not be NULL.
+ *
+ * \return \ref vs_status_e \ref VS_CODE_OK in case of success or error code.
+ */
 vs_status_e
 vs_tl_init(vs_storage_op_ctx_t *op_ctx, vs_hsm_impl_t *hsm);
 
+/** Trust List destruction
+ *
+ * \return \ref vs_status_e \ref VS_CODE_OK in case of success or error code.
+ */
 vs_status_e
 vs_tl_deinit();
 
+/** Trust List element saving
+ *
+ * \param[in] element_info \ref vs_tl_element_info_t element selection. Must not be NULL.
+ * \param[in] in_data Data to be saved. Must not be NULL.
+ * \param[in] data_sz Data size. Must not be zero.
+ *
+ * \return \ref vs_status_e \ref VS_CODE_OK in case of success or error code.
+ */
 vs_status_e
 vs_tl_save_part(vs_tl_element_info_t *element_info, const uint8_t *in_data, uint16_t data_sz);
 
+/** Trust List element loading
+ *
+ * \param[in] element_info \ref vs_tl_element_info_t element selection. Must not be NULL.
+ * \param[out] out_data Output buffer to store data. Must not be NULL.
+ * \param[in] buf_sz Buffer size. Must not be zero.
+ * \param[out] out_sz Pointer to save stored data size. Must not be NULL.
+ *
+ * \return \ref vs_status_e \ref VS_CODE_OK in case of success or error code.
+ */
 vs_status_e
 vs_tl_load_part(vs_tl_element_info_t *element_info, uint8_t *out_data, uint16_t buf_sz, uint16_t *out_sz);
 
+/** Update interface for Trust List
+ *
+ * \return \ref vs_update_interface_t
+ */
 vs_update_interface_t *
 vs_tl_update_ctx(void);
 
+/** Trust List file type
+ *
+ * \return \ref vs_update_file_type_t
+ */
 const vs_update_file_type_t *
 vs_tl_update_file_type(void);
 
+/** Convert Trust List header to host
+ *
+ * \param[in] src_data \ref vs_tl_header_t Data source. Must not be NULL.
+ * \param[out] dst_data \ref vs_tl_header_t Data destination. Must not be NULL.
+ */
 void
 vs_tl_header_to_host(const vs_tl_header_t *src_data, vs_tl_header_t *dst_data);
 
