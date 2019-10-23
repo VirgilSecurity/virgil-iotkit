2cabf2de3eb3fb2b95ca101058d0d41060856a61
git <wizardiche@mail.ru>
2019-10-23 18:33:46 +0500

    Fix size of a build field and descriptor

--- tools/virgil-signer/firmware/firmware.go
+++ tools/virgil-signer/firmware/firmware.go
@@ -35,97 +35,96 @@
 package firmware
 
 import (
-    "bytes"
-    "encoding/binary"
-    "fmt"
+	"bytes"
+	"encoding/binary"
+	"fmt"
 )
 
 const (
-    HEADER_SIZE = 61
-    DESCRIPTOR_SIZE = 44
-    FOOTER_META_SIZE = DESCRIPTOR_SIZE + 1  // descriptor + signatures size
-    SIGNATURE_META_SIZE = 3                 // ec type + key type + hash type
+	HEADER_SIZE         = 59
+	DESCRIPTOR_SIZE     = 42
+	FOOTER_META_SIZE    = DESCRIPTOR_SIZE + 1 // descriptor + signatures size
+	SIGNATURE_META_SIZE = 3                   // ec type + key type + hash type
 )
 
-
-// Size: 4 + 4 + 4 + 4 + 1 + 44 = 61
+// Size: 4 + 4 + 4 + 4 + 1 + 42 = 59
 type Header struct {
-    CodeOffset       uint32
-    CodeLength       uint32
-    FooterOffset     uint32
-    FooterLength     uint32
-    SignaturesCount  uint8
-    Descriptor       Descriptor
+	CodeOffset      uint32
+	CodeLength      uint32
+	FooterOffset    uint32
+	FooterLength    uint32
+	SignaturesCount uint8
+	Descriptor      Descriptor
 }
 
-// Size: 16 + 4 + 13 + 1 + 2 + 4 + 4 = 44
+// Size: 16 + 4 + 11 + 1 + 2 + 4 + 4 = 42
 type Descriptor struct {
-    ManufactureID    [16]byte
-    DeviceType       [4]byte
-    Version          Version
-    Padding          uint8
-    ChunkSize        uint16
-    FirmwareLength   uint32
-    AppSize          uint32
+	ManufactureID  [16]byte
+	DeviceType     [4]byte
+	Version        Version
+	Padding        uint8
+	ChunkSize      uint16
+	FirmwareLength uint32
+	AppSize        uint32
 }
 
-// Size: 4 + 1 + 1 + 1 + 1 + 1 = 9
+// Size: 1 + 1 + 1 + 4 + 4 = 11
 type Version struct {
-    Major           uint8
-    Minor           uint8
-    Patch           uint8
-    Build           uint8
-    Timestamp       uint32
+	Major     uint8
+	Minor     uint8
+	Patch     uint8
+	Build     uint32
+	Timestamp uint32
 }
 
 type Footer struct {
-    SignaturesCount  uint8
-    Descriptor       Descriptor
-    Signatures       []Signature
+	SignaturesCount uint8
+	Descriptor      Descriptor
+	Signatures      []Signature
 }
 
 type Signature struct {
-    SignerType       uint8
-    ECType           uint8
-    HashType         uint8
-    Sign             []byte
-    SignerPublicKey  []byte
+	SignerType      uint8
+	ECType          uint8
+	HashType        uint8
+	Sign            []byte
+	SignerPublicKey []byte
 }
 
 func (s *Signature) ToBytes() ([]byte, error) {
-    buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)
 
-    if err := binary.Write(buf, binary.BigEndian, s.SignerType); err != nil {
-        return nil, fmt.Errorf("failed to serialize Signature SignerType: %v", err)
-    }
-    if err := binary.Write(buf, binary.BigEndian, s.ECType); err != nil {
-        return nil, fmt.Errorf("failed to serialize Signature ECType: %v", err)
-    }
-    if err := binary.Write(buf, binary.BigEndian, s.HashType); err != nil {
-        return nil, fmt.Errorf("failed to serialize Signature HashType: %v", err)
-    }
+	if err := binary.Write(buf, binary.BigEndian, s.SignerType); err != nil {
+		return nil, fmt.Errorf("failed to serialize Signature SignerType: %v", err)
+	}
+	if err := binary.Write(buf, binary.BigEndian, s.ECType); err != nil {
+		return nil, fmt.Errorf("failed to serialize Signature ECType: %v", err)
+	}
+	if err := binary.Write(buf, binary.BigEndian, s.HashType); err != nil {
+		return nil, fmt.Errorf("failed to serialize Signature HashType: %v", err)
+	}
 
-    // Signature
-    if _, err := buf.Write(s.Sign); err != nil {
-        return nil, fmt.Errorf("failed to serialize Signature Sign: %v", err)
-    }
+	// Signature
+	if _, err := buf.Write(s.Sign); err != nil {
+		return nil, fmt.Errorf("failed to serialize Signature Sign: %v", err)
+	}
 
-    // Public key
-    if _, err := buf.Write(s.SignerPublicKey); err != nil {
-        return nil, fmt.Errorf("failed to serialize Signature SignerPublicKey: %v", err)
-    }
+	// Public key
+	if _, err := buf.Write(s.SignerPublicKey); err != nil {
+		return nil, fmt.Errorf("failed to serialize Signature SignerPublicKey: %v", err)
+	}
 
-    return buf.Bytes(), nil
+	return buf.Bytes(), nil
 }
 
 type UpdateFile struct {
-    Header        Header
-    FirmwareCode  []byte
-    Footer        Footer
+	Header       Header
+	FirmwareCode []byte
+	Footer       Footer
 }
 
 type ProgFile struct {
-    FirmwareCode []byte
-    Filler       []byte
-    Footer       Footer
+	FirmwareCode []byte
+	Filler       []byte
+	Footer       Footer
 }
