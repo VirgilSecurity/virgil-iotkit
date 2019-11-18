ba77462a6bfaa594eba39457d97e6aff32ad5cfb
Oleksandr Nemchenko <avn@nemchenko.net>
2019-11-18 17:26:59 +0200

    [doc] Trust List description in depth

--- modules/provision/trust_list/include/virgil/iot/trust_list/trust_list.h
+++ modules/provision/trust_list/include/virgil/iot/trust_list/trust_list.h
@@ -35,7 +35,78 @@
 /*! \file trust_list.h
  * \brief Trust List
  *
+ * This file provides interface for Trust Lists processing.
+ *
+ * Trust List contains the list of trusted public keys and signatures. Initial Trust List is obtained by device during
+ * provision. After it can be upgraded by Update library using FLDT service.
+ *
  * \section trust_list_usage Trust List Usage
+ *
+ * Trust List is maintained by Virgil IoT KID modules and doesn't need to be processed by user. Notwithstanding, there are
+ * some places where it is needed user's attention :
+ *
+ * - Gateway needs to initialize Trust List file type to retransmit it by Update library and to provide #vs_fldt_server_add_filetype_cb
+ * for FLDT Server serviceц:
+ *
+ * \code
+ *
+ * vs_status_e
+ * _add_filetype(const vs_update_file_type_t *file_type, vs_update_interface_t **update_ctx) {
+ *     switch (file_type->type) {
+ *
+ *     // ...
+ *
+ *     // Trust List file type
+ *         case VS_UPDATE_TRUST_LIST:
+ *             *update_ctx = vs_tl_update_ctx();
+ *             break;
+ *     }
+ *
+ *     return VS_CODE_OK;
+ * }
+ *
+ * // ...
+ *
+ *     const vs_snap_service_t *snap_fldt_server;      // FLDT Server service
+ *     vs_mac_addr_t mac_addr;                         // Own MAC address
+ *
+ *     // Initialize mac_addr
+ *
+ *     //  FLDT server service
+ *     snap_fldt_server = vs_snap_fldt_server(&mac_addr, _add_filetype);
+ *     STATUS_CHECK(vs_snap_register_service(snap_fldt_server), "Cannot register FLDT server service");
+ *     STATUS_CHECK(vs_fldt_server_add_file_type(vs_tl_update_file_type(), vs_tl_update_ctx(), false),
+ *                  "Unable to add Trust List file type");
+ *     // Other file types
+ *
+ * \endcode
+ *
+ * - After receiving by Gateway new Trust List by Cloud library it is necessary to send its version to Things. For this
+ * goal #vs_tl_load_part is used. It reads Trust List header :
+ *
+ * \code
+ *
+ *  vs_tl_element_info_t elem = {.id = VS_TL_ELEMENT_TLH};      // Trust List header to be read
+ *  vs_tl_header_t tl_header;                                   // Header
+ *  uint16_t tl_header_sz = sizeof(tl_header);                  // Header size
+ *  vs_update_file_type_t tl_info;                              // Trust List file update information
+ *
+ *  // Load latest local Trust List
+ *  STATUS_CHECK(vs_tl_load_part(&elem, (uint8_t *)&tl_header, tl_header_sz, &tl_header_sz) &&
+ *       tl_header_sz == sizeof(tl_header), "Unable to load Trust List header");
+ *  tl_header.version.build = ntohl(tl_header.version.build);
+ *
+ *  // Prepare Update information
+ *  memset(tl_info, 0, sizeof(vs_update_file_type_t));
+ *  memcpy(&tl_info.info.version, &tl_header.version, sizeof(vs_file_version_t));
+ *
+ *  tl_info.type = VS_UPDATE_TRUST_LIST;
+ *
+ *  // Broadcast new Trust List information
+ *  STATUS_CHECK(vs_fldt_server_add_file_type(&tl_info, vs_tl_update_ctx(), true),
+ *          "Unable to add new Trust List");
+ *
+ * \endcode
  */
 
 #ifndef TRUST_LIST_H
@@ -51,6 +122,10 @@
 
 /** Trust List initialization
  *
+ * Initializes Trust List.
+ *
+ * \note It is called by #vs_provision_init.
+ *
  * \param[in] op_ctx Storage context. Must not be NULL.
  * \param[in] secmodule Security Module implementation. Must not be NULL.
  *
@@ -61,12 +136,18 @@ vs_tl_init(vs_storage_op_ctx_t *op_ctx, vs_secmodule_impl_t *secmodule);
 
 /** Trust List destruction
  *
+ * Destroys Trust List.
+ *
+ * \note It is called by #vs_provision_deinit.
+ *
  * \return #VS_CODE_OK in case of success or error code.
  */
 vs_status_e
 vs_tl_deinit();
 
 /** Trust List element saving
+ *
+ * Saves Trust List header, footer or data chunk. Element selection it is done by \a element_info selector.
  *
  * \param[in] element_info Element selection. Must not be NULL.
  * \param[in] in_data Data to be saved. Must not be NULL.
@@ -78,6 +159,8 @@ vs_status_e
 vs_tl_save_part(vs_tl_element_info_t *element_info, const uint8_t *in_data, uint16_t data_sz);
 
 /** Trust List element loading
+ *
+ * Loads Trust List header, footer or data chunk. Element selection it is done by \a element_info selector.
  *
  * \param[in] element_info Element selection. Must not be NULL.
  * \param[out] out_data Output buffer to store data. Must not be NULL.
@@ -91,12 +174,16 @@ vs_tl_load_part(vs_tl_element_info_t *element_info, uint8_t *out_data, uint16_t 
 
 /** Update interface for Trust List
  *
+ * Returns Update context for Trust List.
+ *
  * \return #vs_update_interface_t
  */
 vs_update_interface_t *
 vs_tl_update_ctx(void);
 
 /** Trust List file type
+ *
+ * Returns Trust List file type for Update module.
  *
  * \return #vs_update_file_type_t
  */
@@ -105,12 +192,22 @@ vs_tl_update_file_type(void);
 
 /** Convert Trust List header to host
  *
+ * Convert Trust List header to the host format.
+ *
  * \param[in] src_data Data source. Must not be NULL.
  * \param[out] dst_data Data destination. Must not be NULL.
  */
 void
 vs_tl_header_to_host(const vs_tl_header_t *src_data, vs_tl_header_t *dst_data);
 
+/** Convert Trust List header to network
+ *
+ * Convert Trust List header to the network format.
+ *
+ * \param[in] src_data Data source. Must not be NULL.
+ * \param[out] dst_data Data destination. Must not be NULL.
+ */
 void
 vs_tl_header_to_net(const vs_tl_header_t *src_data, vs_tl_header_t *dst_data);
+
 #endif // TRUST_LIST_H
