63b93b38fe720f5850aac796531122ac6bd4feed
Oleksandr Nemchenko <avn@nemchenko.net>
2019-11-20 18:52:00 +0200

    Documentation changes for commit 'Merge branch 'develop-readme-review' of https://github.com/VirgilSecurity/virgil-iot-sdk into develop' 277ddf81ef8f14a6f35d782905f61d9e90728269

--- v0.1.0-alpha/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['jobj_5ft_816',['jobj_t',['../structjobj__t.html',1,'']]],
-  ['json_5fstr_817',['json_str',['../structjson__str.html',1,'']]]
-];
--- v0.1.0-alpha/search/files_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['global_2dhal_2eh_866',['global-hal.h',['../global-hal_8h.html',1,'']]]
-];
