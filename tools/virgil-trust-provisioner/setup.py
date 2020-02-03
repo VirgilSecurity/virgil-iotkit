282e6d298fad33c33dab753e82edebf1242bf8aa
Dmytro Hrytsenko <dm.hrytsenko@gmail.com>
2020-02-03 22:40:37 +0200

    Update dependencies versions for Virgil Trust Provisioner

--- tools/virgil-trust-provisioner/setup.py
+++ tools/virgil-trust-provisioner/setup.py
@@ -8,10 +8,10 @@ setup(
     install_requires=[
         'virgil-sdk==5.2.1',
         'virgil-crypto>=3,<4',
-        'prettytable==0.7.2',
-        'pyasn1==0.4.8',
-        'psutil==5.6.7',
-        'tinydb==3.15.2'
+        'prettytable>0.7,<0.8',
+        'pyasn1>0.4,<0.5',
+        'psutil>5,<6',
+        'tinydb>3,<4'
     ],
     entry_points={
         'console_scripts':
