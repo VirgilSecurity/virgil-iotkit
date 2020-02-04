0626b33dbe000564ea55fc745eaee25521bc3dda
Roman Kutashenko <kutashenko@gmail.com>
2020-02-05 00:07:26 +0200

    Print ifconfig -a

--- scripts/sandbox/run.sh
+++ scripts/sandbox/run.sh
@@ -3,6 +3,7 @@
 IMAGE="virgilsecurity-docker-iotl-demo.bintray.io/testing/iot-sdk-demo:0.1.0.63"
 
 if [ "${1}" = "vagrant" ]; then
+    ifconfig -a
     SUBNET_BCAST=$(ifconfig eth1 | grep -Eo 'broadcast (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
     echo "SUBNET_BCAST = ${SUBNET_BCAST}"
     exec docker run -it --rm -p 8080:8080 -p 8081:8081 --net=host -e VS_BCAST_SUBNET_ADDR="${SUBNET_BCAST}" ${IMAGE}
