9cc82a0d6137aaa2cd2aaaad1c021e2c5e02e983
Roman Kutashenko <kutashenko@gmail.com>
2020-02-04 23:44:38 +0200

    Update Sandbox image

--- scripts/sandbox/run.sh
+++ scripts/sandbox/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE="virgilsecurity-docker-iotl-demo.bintray.io/testing/iot-sdk-demo:0.1.0.59"
+IMAGE="virgilsecurity-docker-iotl-demo.bintray.io/testing/iot-sdk-demo:0.1.0.63"
 
 if [ "${1}" = "vagrant" ]; then
     SUBNET_BCAST=$(ifconfig eth1 | grep -Eo 'broadcast (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
@@ -10,8 +10,3 @@ else
     echo "SUBNET_BCAST = 255.255.255.255"
     exec docker run -it --rm -p 8080:8080 -p 8081:8081 --net=host ${IMAGE}
 fi
-
-
-
-
-
