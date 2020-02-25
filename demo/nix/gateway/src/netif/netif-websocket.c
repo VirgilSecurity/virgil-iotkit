0468b9bcf4479ad0256fc6c02a76ca38ef70adb5
git <wizardiche@mail.ru>
2020-02-25 18:34:58 +0500

    Fix build

--- demo/nix/gateway/src/netif/netif-websocket.c
+++ demo/nix/gateway/src/netif/netif-websocket.c
@@ -507,7 +507,7 @@ _message_queue_processing(bool *is_subscr_msg_sent) {
     vs_netif_t *netif = 0;
     const uint8_t *data = 0;
     size_t data_sz = 0;
-    bool ret;
+    vs_status_e ret;
 
     VS_IOT_ASSERT(_queue_ctx);
     if (!_queue_ctx) {
@@ -534,15 +534,16 @@ _message_queue_processing(bool *is_subscr_msg_sent) {
     }
 
     while (vs_msg_queue_data_present(_queue_ctx)) {
+        bool result;
         CHECK_RET(VS_CODE_OK == vs_msg_queue_pop(_queue_ctx, (const void **)&netif, &data, &data_sz),
                   VS_CODE_ERR_QUEUE,
                   "Error while reading message from queue");
 
-//        VS_LOG_DEBUG("Send message = %s", (char *)data);
-        ret = cws_send_text(_websocket_ctx.easy, (char *)data);
+        //        VS_LOG_DEBUG("Send message = %s", (char *)data);
+        result = cws_send_text(_websocket_ctx.easy, (char *)data);
         free((void *)data);
 
-        if (!ret) {
+        if (!result) {
             vs_msg_queue_reset(_queue_ctx);
             return VS_CODE_ERR_SOCKET;
         }
