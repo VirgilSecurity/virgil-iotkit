affd05abe1e9eac15e5da20d5b0d4bb78cc798eb
Dmytro Hrytsenko <dm.hrytsenko@gmail.com>
2020-03-09 15:10:14 +0200

    Update scripts for Sandbox launch

--- scripts/sandbox/run.sh
+++ scripts/sandbox/run.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-IMAGE="virgilsecurity-docker-iotl-demo.bintray.io/testing/iot-sdk-demo:0.1.3.70"
+IMAGE="virgilsecurity-docker-iotl-demo.bintray.io/testing/iot-sdk-demo:0.1.4.5"
 
+#
+#   Ask for Application token
+#
+read -p 'Please enter Virgil Application Token: ' APP_TOKEN
+
+#
+#   Start Sandbox
+#
 if [ "${1}" = "vagrant" ]; then
     sleep 5s
     ifconfig -a
     SUBNET_BCAST=$(ifconfig eth1 | grep -Eo 'broadcast (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
     echo "SUBNET_BCAST = ${SUBNET_BCAST}"
-    sleep 3s
-    exec docker run -it --rm -p 8080:8080 -p 8081:8081 --net=host -e VS_BCAST_SUBNET_ADDR="${SUBNET_BCAST}" ${IMAGE}
+    EXTRA_OPTIONS="-e VS_BCAST_SUBNET_ADDR='${SUBNET_BCAST}'"
 else
     echo "SUBNET_BCAST = 255.255.255.255"
-    exec docker run -it --rm -p 8080:8080 -p 8081:8081 --net=host ${IMAGE}
 fi
+
+exec docker run -it --rm \
+    -e LOCAL_MODE=true \
+    -e APP_TOKEN=${APP_TOKEN} \
+    -p 8000:8000 \
+    -p 8080:8080 \
+    -p 8081:8081 \
+    ${EXTRA_OPTIONS} \
+    --net=host ${IMAGE}
