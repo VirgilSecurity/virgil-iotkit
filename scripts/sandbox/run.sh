f11d2861ae6976afbb2a6cb73b5d321968b55bab
Dmytro Hrytsenko <dm.hrytsenko@gmail.com>
2020-03-10 12:47:00 +0200

    Update IoT Sandbox image

--- scripts/sandbox/run.sh
+++ scripts/sandbox/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE="virgilsecurity-docker-iotl-demo.bintray.io/testing/iot-sdk-demo:0.1.4.5"
+IMAGE="virgilsecurity-docker-iotl-demo.bintray.io/testing/iot-sdk-demo:0.1.4.6"
 
 #
 #   Ask for Application token
