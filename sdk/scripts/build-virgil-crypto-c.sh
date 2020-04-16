54addceabd2fe76b0dc4a03ff2e50bccd29b6394
Roman Kutashenko <kutashenko@gmail.com>
2020-04-16 09:17:45 +0300

    Possibility to skip parts to be built

--- sdk/scripts/build-virgil-crypto-c.sh
+++ sdk/scripts/build-virgil-crypto-c.sh
@@ -11,16 +11,16 @@ BUILD_DIR_BASE="${CRYPTO_C_DIR}"
 CMAKE_CUSTOM_PARAM="${@}"
 
 if [[ $@ == *"mingw32.toolchain.cmake"* ]]; then
-  AR_TOOLS="i686-w64-mingw32-ar"
-  OBJ_EXT="obj"
-elif [[ $@ == *"android.toolchain.cmake"* ]]; then
-  AR_TOOLS="${AR_TOOLS_ANDROID}"
-  OBJ_EXT="o"
-  echo "AR_TOOLS = $AR_TOOLS"
+    AR_TOOLS="i686-w64-mingw32-ar"
+    OBJ_EXT="obj"
+    elif [[ $@ == *"android.toolchain.cmake"* ]]; then
+    AR_TOOLS="${AR_TOOLS_ANDROID}"
+    OBJ_EXT="o"
+    echo "AR_TOOLS = $AR_TOOLS"
 else
-  AR_TOOLS="ar"
-  OBJ_EXT="o"
-  [ "$(arch)" == "x86_64" ] && LIB_ARCH="64" || LIB_ARCH=""
+    AR_TOOLS="ar"
+    OBJ_EXT="o"
+    [ "$(arch)" == "x86_64" ] && LIB_ARCH="64" || LIB_ARCH=""
 fi
 
 #
@@ -36,46 +36,49 @@ function build() {
     local BUILD_TYPE=$1
     local CMAKE_ARGUMENTS=$2
     local CORES=10
-
+    
     local BUILD_DIR=${BUILD_DIR_BASE}/cmake-build-${BUILD_DIR_SUFFIX}/${BUILD_TYPE}
     local INSTALL_DIR=${QT_INSTALL_DIR_BASE}/${BUILD_DIR_SUFFIX}/${BUILD_TYPE}/installed
     local LIBS_DIR=${INSTALL_DIR}/usr/local/lib${LIB_ARCH}
-
+    
     echo
     echo "===================================="
     echo "=== ${BUILD_DIR_SUFFIX} ${BUILD_TYPE} build"
     echo "=== Output directory: ${BUILD_DIR}"
     echo "===================================="
     echo
-
+    
     rm -rf ${BUILD_DIR}
     mkdir -p ${BUILD_DIR}
     mkdir -p ${INSTALL_DIR}
-
+    
     pushd ${BUILD_DIR}
-      # prepare to build
-      echo "==========="
-      echo "=== Run CMAKE "
-      echo "==========="
-      cmake ${BUILD_DIR_BASE} ${CMAKE_ARGUMENTS} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G "Unix Makefiles"
-      check_error
-
-      # build all targets
-      echo "==========="
-      echo "=== Building"
-      echo "==========="
-      make -j ${CORES}
-      check_error
-
-      # install all targets
-      echo "==========="
-      echo "=== Installing"
-      echo "==========="
-      make DESTDIR=${INSTALL_DIR} install
-      check_error
-
-      pack_libs ${LIBS_DIR} "libed25519.a libmbedcrypto.a libprotobuf-nanopb.a libvsc_common.a libvsc_foundation.a libvsc_foundation_pb.a" "libvscryptoc.a"
-
+    # prepare to build
+    echo "==========="
+    echo "=== Run CMAKE "
+    echo "==========="
+    cmake ${BUILD_DIR_BASE} ${CMAKE_ARGUMENTS} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G "Unix Makefiles"
+    check_error
+    
+    # build all targets
+    echo "==========="
+    echo "=== Building"
+    echo "==========="
+    make -j ${CORES}
+    check_error
+    
+    # install all targets
+    echo "==========="
+    echo "=== Installing"
+    echo "==========="
+    make DESTDIR=${INSTALL_DIR} install
+    check_error
+    
+    pack_libs ${LIBS_DIR} "libed25519.a libmbedcrypto.a libprotobuf-nanopb.a libvsc_common.a libvsc_foundation.a libvsc_foundation_pb.a" "libvscryptoc.a"
+    
+    # Clean
+    rm -rf ${INSTALL_DIR}/${HOME}/../
+    
     popd
 }
 
