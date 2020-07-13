58af4a74a8a03e80656b90730e4d71ea3802f089
Denis Kurilenko <kurilenkodenis@gmail.com>
2020-07-13 15:03:46 +0300

    Revert "Change snapshot of card request"

--- tools/virgil-device-initializer/request/request.go
+++ tools/virgil-device-initializer/request/request.go
@@ -35,133 +35,130 @@
 package request
 
 import (
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"time"
+    "encoding/hex"
+    "encoding/json"
+    "fmt"
+    "time"
 
-	"../common"
-	"../converters"
-	"../snap"
+    "../common"
+    "../converters"
+    "../snap"
 
-	"gopkg.in/virgil.v5/sdk"
+    "gopkg.in/virgil.v5/sdk"
 )
 
 type Builder struct {
-	Signer          common.SignerInterface
-	DeviceProcessor *snap.DeviceProcessor
+    Signer          common.SignerInterface
+    DeviceProcessor *snap.DeviceProcessor
 
-	deviceInfo *DeviceInfoJson
+    deviceInfo      *DeviceInfoJson
 }
 
 type DeviceInfoJson struct {
-	Manufacturer  []byte   `json:"manufacturer"`
-	Model         []byte   `json:"model"`
-	Roles         []string `json:"roles"`
-	Mac           string   `json:"mac"`
-	Serial        []byte   `json:"serial"`
-	PublicKeyTiny []byte   `json:"public_key_tiny"`
-	Signature     []byte   `json:"signature"`
-	FactoryId     []byte   `json:"factory_id"`
-	KeyType       uint8    `json:"key_type"`
-	ECType        uint8    `json:"ec_type"`
+    Manufacturer  string      `json:"manufacturer"`
+    Model         string      `json:"model"`
+    Roles         []string    `json:"roles"`
+    Mac           string      `json:"mac"`
+    Serial        []byte      `json:"serial"`
+    PublicKeyTiny []byte      `json:"publicKeyTiny"`
+    Signature     []byte      `json:"signature"`
+    KeyType       uint8       `json:"key_type"`
+    ECType        uint8       `json:"ec_type"`
 }
 
 type CardSnapshotJson struct {
-	Device string `json:"device"`
-	*DeviceInfoJson
+    Device        string      `json:"device"`
+    DeviceName    string      `json:"device_name"`
+    *DeviceInfoJson
 }
 
 func (b Builder) BuildRequest() (string, error) {
-	var err error
+    var err error
 
-	identity := hex.EncodeToString(b.DeviceProcessor.Serial[:])
+    identity := hex.EncodeToString(b.DeviceProcessor.Serial[:])
 
-	// Convert raw public key to Virgil format
-	var virgilPubKey []byte
-	virgilPubKey, err = converters.RawPubKeyToVirgil(b.DeviceProcessor.DevicePublicKey.RawPubKey,
-		b.DeviceProcessor.DevicePublicKey.ECType)
-	if err != nil {
-		return "", err
-	}
+    // Convert raw public key to Virgil format
+    var virgilPubKey []byte
+    virgilPubKey, err = converters.RawPubKeyToVirgil(b.DeviceProcessor.DevicePublicKey.RawPubKey,
+                                                     b.DeviceProcessor.DevicePublicKey.ECType)
+    if err != nil {
+        return "", err
+    }
 
-	// Prepare card content snapshot
-	cardContent := sdk.RawCardContent{
-		Identity:  identity,
-		PublicKey: virgilPubKey,
-		CreatedAt: time.Now().UTC().Unix(),
-		Version:   sdk.CardVersion,
-	}
-	cardContentSnapshot, err := sdk.TakeSnapshot(cardContent)
-	if err != nil {
-		return "", fmt.Errorf("failed to take content snapshot: %v", err)
-	}
+    // Prepare card content snapshot
+    cardContent := sdk.RawCardContent{
+        Identity:       identity,
+        PublicKey:      virgilPubKey,
+        CreatedAt:      time.Now().UTC().Unix(),
+        Version:        sdk.CardVersion,
+    }
+    cardContentSnapshot, err := sdk.TakeSnapshot(cardContent)
+    if err != nil {
+        return "", fmt.Errorf("failed to take content snapshot: %v", err)
+    }
 
-	// Create card
-	rawCard := sdk.RawSignedModel{
-		ContentSnapshot: cardContentSnapshot,
-	}
+    // Create card
+    rawCard := sdk.RawSignedModel{
+        ContentSnapshot: cardContentSnapshot,
+    }
 
-	// Sign combined snapshot inside device
-	extraContent, err := b.GetCardSnapshot()
-	if err != nil {
-		return "", fmt.Errorf("failed to get device info: %v", err)
-	}
-	combinedSnapshot := append(rawCard.ContentSnapshot, extraContent...)
-	signature, err := b.Signer.Sign(combinedSnapshot)
-	if err != nil {
-		return "", err
-	}
-	rawSignature := sdk.RawCardSignature{
-		Signer:    "self",
-		Signature: signature,
-		Snapshot:  extraContent,
-	}
+    // Sign combined snapshot inside device
+    extraContent, err := b.GetCardSnapshot()
+    if err != nil {
+        return "", fmt.Errorf("failed to get device info: %v", err)
+    }
+    combinedSnapshot := append(rawCard.ContentSnapshot, extraContent...)
+    signature, err := b.Signer.Sign(combinedSnapshot)
+    if err != nil {
+        return "", err
+    }
+    rawSignature := sdk.RawCardSignature{
+        Signer:    "self",
+        Signature: signature,
+        Snapshot:  extraContent,
+    }
 
-	// Append signature to card
-	rawCard.Signatures = append(rawCard.Signatures, &rawSignature)
+    // Append signature to card
+    rawCard.Signatures = append(rawCard.Signatures, &rawSignature)
 
-	// Export card as base64
-	rawCardBase64, err := rawCard.ExportAsBase64EncodedString()
-	if err != nil {
-		return "", fmt.Errorf("failed to export card as base64: %v", err)
-	}
-	return rawCardBase64, nil
+    // Export card as base64
+    rawCardBase64, err := rawCard.ExportAsBase64EncodedString()
+    if err != nil {
+        return "", fmt.Errorf("failed to export card as base64: %v", err)
+    }
+    return rawCardBase64, nil
 }
 
 func (b *Builder) GetDeviceInfo() ([]byte, error) {
-	mac := b.DeviceProcessor.DeviceMacAddr
-
-	info := &DeviceInfoJson{
-		Manufacturer:  b.DeviceProcessor.Manufacturer[:],
-		Model:         b.DeviceProcessor.Model[:],
-		Mac:           fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]),
-		Serial:        b.DeviceProcessor.Serial[:],
-		PublicKeyTiny: b.DeviceProcessor.DevicePublicKey.RawPubKey,
-		Signature:     b.DeviceProcessor.Signature.RawSignature,
-		FactoryId:     b.DeviceProcessor.FactoryId[:],
-		KeyType:       b.DeviceProcessor.DevicePublicKey.KeyType,
-		ECType:        b.DeviceProcessor.DevicePublicKey.ECType,
-		Roles:         b.DeviceProcessor.Roles,
-	}
-	b.deviceInfo = info
-	marshaled, err := json.Marshal(info)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal DeviceInfoJson: %v", err)
-	}
-
-	return marshaled, nil
+    mac := b.DeviceProcessor.DeviceMacAddr
+    info := &DeviceInfoJson{
+        Manufacturer:    fmt.Sprintf("%#x", b.DeviceProcessor.Manufacturer),
+        Model:           fmt.Sprintf("%#x", b.DeviceProcessor.Model),
+        Mac:             fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]),
+        Serial:          b.DeviceProcessor.Serial[:],
+        PublicKeyTiny:   b.DeviceProcessor.DevicePublicKey.RawPubKey,
+        Signature:       b.DeviceProcessor.Signature.RawSignature,
+        KeyType:         b.DeviceProcessor.DevicePublicKey.KeyType,
+        ECType:          b.DeviceProcessor.DevicePublicKey.ECType,
+        Roles:           b.DeviceProcessor.Roles,
+    }
+    b.deviceInfo = info
+    marshaled, err := json.Marshal(info)
+    if err != nil {
+        return nil, fmt.Errorf("failed to marshal DeviceInfoJson: %v", err)
+    }
+    return marshaled, nil
 }
 
 func (b *Builder) GetCardSnapshot() ([]byte, error) {
-	deviceInfo := CardSnapshotJson{
-		Device:         "",
-		DeviceInfoJson: b.deviceInfo,
-	}
-	marshaled, err := json.Marshal(deviceInfo)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal CardSnapshotJson: %v", err)
-	}
-
-	return marshaled, nil
+    deviceInfo := CardSnapshotJson{
+        Device:         "",
+        DeviceName:     fmt.Sprint(b.DeviceProcessor.Model),
+        DeviceInfoJson: b.deviceInfo,
+    }
+    marshaled, err := json.Marshal(deviceInfo)
+    if err != nil {
+        return nil, fmt.Errorf("failed to marshal CardSnapshotJson: %v", err)
+    }
+    return marshaled, nil
 }
