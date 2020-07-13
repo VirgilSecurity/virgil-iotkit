f8961da01d4d45d6bd50cf2ba55325b349fa3cab
lantaris <edik.ponomarenko@mail.ru>
2020-07-13 12:49:03 +0300

    Fix build internal

--- sdk/modules/messenger/internal/src/virgil.c
+++ sdk/modules/messenger/internal/src/virgil.c
@@ -2009,6 +2009,17 @@ vs_logger_output_hal(const char *buffer) {
 }
 
 /******************************************************************************/
+bool
+vs_logger_current_time_hal(void) {
+    time_t result = time(NULL);
+    if (result != -1) {
+        printf("%s", asctime(gmtime(&result)));
+        return true;
+    }
+    return false;
+}
+
+/******************************************************************************/
 DLL_PUBLIC vs_status_e
 vs_messenger_virgil_search(const char *identity) {
     //
